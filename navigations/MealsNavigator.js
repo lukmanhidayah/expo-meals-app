247fa69a2d4304b354fc3ce9e92eaa4fb5eba0a5
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-09-03 21:23:50 +0700

    Adding tabs bottom navigator

--- navigations/MealsNavigator.js
+++ navigations/MealsNavigator.js
@@ -1,11 +1,16 @@
+import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Platform } from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
 
 //all screens needed
 import Categories from "../screens/Categories.screen";
 import CatogoryMeals from "../screens/CatogoryMeals.Screen";
 import MealDetail from "../screens/MealDetail.screen";
+import Favorites from "../screens/Favorites.screen";
 
 //all constants needed
 import Colors from "../constans/Colors";
@@ -37,4 +42,39 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name={"ios-restaurant"}
+              size={24}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    Favorites: {
+      screen: Favorites,
+      navigationOptions: {
+        tabBarLabel: "Favorites!",
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name={"ios-star"} size={24} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigator);
